ca1d3537d61e115a8ba2ca48a0937e7a5cec817a
julia951 <klimuk.julia@gmail.com>
2022-06-28 17:59:43 +0300

    first commit

---
+++ src/test/java/ParamTest.java
@@ -0,0 +1,83 @@
+import com.codeborne.selenide.CollectionCondition;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import static org.junit.jupiter.api.Assertions.*;
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static java.util.Arrays.asList;
+public class ParamTest {
+
+    @ParameterizedTest(name = "При поиске слова - {0} отображаются статьи про {0}")
+    @ValueSource(strings = {"qa", "qc", "testing"})
+    void testGoogleSearch(String searchItem) {
+        open("https://www.google.com/");
+        $(".gLFyf.gsfi").setValue(searchItem);
+        $(".gNO89b").pressEnter();
+        $$(".jtfYYd").find((text(searchItem))).shouldBe(visible);
+    }
+
+    @CsvSource(value = {
+            "qa, самое широкое из всех понятий и представляет собой совокупность мероприятий",
+            "qc, це процес знаходження помилок в продукті",
+            "testing, delegation would like to urge those States"})
+    @ParameterizedTest(name = "При поиске слова - {0} отображаются статьи про {1}")
+    void testGoogleSearchCSV(String searchItem) {
+        open("https://www.google.com/");
+        $(".gLFyf.gsfi").setValue(searchItem);
+        $(".gNO89b").pressEnter();
+        $$(".jtfYYd").find((text(searchItem))).shouldBe(visible);
+    }
+
+    @CsvFileSource(resources = "testData/forGoogle")
+    @ParameterizedTest(name = "При поиске слова - {0} отображаются статьи про {1}")
+    void testGoogleSearchCSVFile(String searchItem) {
+        open("https://www.google.com/");
+        $(".gLFyf.gsfi").setValue(searchItem);
+        $(".gNO89b").pressEnter();
+        $$(".jtfYYd").find((text(searchItem))).shouldBe(visible);
+    }
+
+    static Stream<Arguments>googleItemSearch(){
+        return Stream.of(
+                Arguments.of("qa", asList("Quality assurance")),
+                Arguments.of("qc", asList("Quality Control")),
+                Arguments.of("testing", asList("Software testing")
+                ));
+    }
+    @MethodSource(value = "googleItemSearch")
+    @ParameterizedTest(name = "При поиске слова - {0} отображаются статьи про {1}")
+    void testGoogleSearchStream(String searchItem, List<String> expectedResult) {
+        open("https://www.google.com/");
+        $(".gLFyf.gsfi").setValue(searchItem);
+        $(".gNO89b").pressEnter();
+        $$(".jtfYYd").shouldHave(CollectionCondition.texts(expectedResult));
+    }
+
+    @EnumSource(enumExamples.class)
+    @ParameterizedTest
+    void enumTest(enumExamples enumExamples) {
+        open("https://www.google.com/");
+        $(".gLFyf.gsfi").setValue(enumExamples.desc);
+        $(".gNO89b").pressEnter();
+        $$(".jtfYYd").shouldHave(CollectionCondition.texts(enumExamples.desc));
+    }
+
+}
---
+++ src/test/java/enumExamples.java
@@ -0,0 +1,13 @@
+public enum enumExamples {
+
+    QA ("qa"),
+    QC ("qc"),
+    TESTING ("testing");
+
+    public final String desc;
+
+    enumExamples (String desc) {
+        this.desc = desc;
+    }
+
+}
